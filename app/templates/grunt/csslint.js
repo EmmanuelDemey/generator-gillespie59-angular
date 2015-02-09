a87f65b21e24026421e64f34c75846941902c150
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-02-09 20:21:32 +0100

    Add CSSLint grunt plugin

---
+++ app/templates/grunt/csslint.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+	/**
+	* Check the quality of our JS files
+	*/
+	module.exports = {
+		app: {
+			options: {
+			},
+			src: ['app/styles/*.css']
+		}
+	};
+})();
